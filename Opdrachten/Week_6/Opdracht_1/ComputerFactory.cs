df3e9e52eab88ff0d42f6d165d37d6478db380fe
JesseTheRookie <jessesmail.jve@gmail.com>
2019-03-18 15:40:15 +0100

    Begin week 6 opdracht. week 5 opdracht 2 nog niet af.

---
+++ Opdrachten/Week_6/Opdracht_1/ComputerFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht_1
+{
+    abstract class ComputerFactory
+    {
+        protected abstract IHardDisk MakeHardDisk();
+
+        protected virtual IMonitor MakeMonitor()
+        {
+            return new Monitor();
+        }
+        protected abstract IProcessor MakeProcessor();
+    
+    }
+    public class HardDisk : IHardDisk
+    {
+        public virtual void StoreData()
+        {
+            Console.WriteLine("Stores data");
+        }
+    }
+    public class Monitor : IMonitor
+    {
+        public void Display()
+        {
+            Console.WriteLine("Displaying stuff");
+        }
+    }
+    public class Processor : IProcessor
+    {
+        public void PerformOperation()
+        {
+            Console.WriteLine("Processing stuff");
+        }
+    }
+}
